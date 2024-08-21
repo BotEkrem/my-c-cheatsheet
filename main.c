3e8c7a62ed45e1352bc27e6311018352c6cfdcc2
Ekrem <42387878+BotEkrem@users.noreply.github.com>
2024-08-21 23:07:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -62,14 +62,14 @@ int main(void) {
 
     /* Arithmetic Operators
 
-    Operator	Name	          Description                                 Example
-    +	        Addition	      Adds together two values	                  x + y
-    -	        Subtraction	    Subtracts one value from another	          x - y
-    *	        Multiplication  Multiplies two values	                      x * y
-    /	        Division	      Divides one value by another	              x / y
+    Operator	Name	        Description                                 Example
+    +	        Addition	    Adds together two values	                x + y
+    -	        Subtraction	    Subtracts one value from another	        x - y
+    *	        Multiplication  Multiplies two values	                    x * y
+    /	        Division	    Divides one value by another	            x / y
     %	        Modulus	        Returns the division remainder	            x % y
-    ++	      Increment	      Increases the value of a variable by 1      ++x
-    --	      Decrement	      Decreases the value of a variable by 1      --x
+    ++	        Increment	    Increases the value of a variable by 1      ++x
+    --	        Decrement	    Decreases the value of a variable by 1      --x
 
     */
 
@@ -93,8 +93,8 @@ int main(void) {
 
     /* Assignment Operators
 
-    Operator	  Example	        Same As
-    =  	        x = 5	          x = 5
+    Operator	Example	        Same As
+    =  	        x = 5	        x = 5
     += 	        x += 3	        x = x + 3
     -= 	        x -= 3	        x = x - 3
     *= 	        x *= 3	        x = x * 3
@@ -135,11 +135,11 @@ int main(void) {
 
     /* Comparison Operators
 
-    Operator	  Name	                    Example	        Description
+    Operator	Name	                    Example	        Description
     ==	        Equal to	                x == y	        Returns 1 if the values are equal
     !=	        Not equal	                x != y	        Returns 1 if the values are not equal
-    >	          Greater than	            x > y	          Returns 1 if the first value is greater than the second value
-    <	          Less than	                x < y	          Returns 1 if the first value is less than the second value
+    >	        Greater than	            x > y	        Returns 1 if the first value is greater than the second value
+    <	        Less than	                x < y	        Returns 1 if the first value is less than the second value
     >=	        Greater than or equal to	x >= y	        Returns 1 if the first value is greater than, or equal to, the second value
     <=	        Less than or equal to	    x <= y	        Returns 1 if the first value is less than, or equal to, the second value
 
@@ -162,9 +162,9 @@ int main(void) {
 
     /* Logical Operators
 
-    Operator	Name	  Example	            Description
-    && 	      AND	    x < 5 &&  x < 10	  Returns 1 if both statements are true
-    || 	      OR	    x < 5 || x < 4	    Returns 1 if one of the statements is true
+    Operator	Name	Example	            Description
+    && 	        AND	    x < 5 &&  x < 10	Returns 1 if both statements are true
+    || 	        OR	    x < 5 || x < 4	    Returns 1 if one of the statements is true
     !	        NOT	    !(x < 5 && x < 10)	Reverse the result, returns 0 if the result is 1
 
     */
@@ -208,34 +208,34 @@ int main(void) {
 
     switch (input[0] - '0') /* char to int */ {
         case 0:
-            printf("Zero\n");
+            printf("Zero");
             break;
         case 1:
-            printf("One\n");
+            printf("One");
         break;
         case 2:
-            printf("Two\n");
+            printf("Two");
         break;
         case 3:
-            printf("Three\n");
+            printf("Three");
         break;
         case 4:
-            printf("Four\n");
+            printf("Four");
         break;
         case 5:
-            printf("Five\n");
+            printf("Five");
         break;
         case 6:
-            printf("Six\n");
+            printf("Six");
         break;
         case 7:
-            printf("Seven\n");
+            printf("Seven");
         break;
         case 8:
-            printf("Eight\n");
+            printf("Eight");
         break;
         case 9:
-            printf("Nine\n");
+            printf("Nine");
         break;
         default:
             printf("No case triggered.\n");
